897e3a2cbd8e7da0e3a117ad1e967d6b2a3bb2df
cristianomarianoufsc-ops <231576260+cristianomarianoufsc-ops@users.noreply.github.com>
2025-12-10 09:06:43 -0500

    fix: Corrige erro 'autoTable is not a function' alterando a importação do jsPDF para default.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,4 +1,4 @@
-import { jsPDF } from "jspdf";
+import jsPDF from "jspdf";
 import "jspdf-autotable";
 import React, { useState, useEffect, useMemo } from "react";
 import ReactDOM from "react-dom";
